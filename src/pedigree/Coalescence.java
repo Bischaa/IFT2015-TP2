fa2b15122391436380433a644382ab8636dded71
Maxime Ton <maximeton@gmail.com>
2020-02-24 13:41:18 -0500

    Terminé et testé Partie B, lancer XYLineChart_AWT avec les .jar dans lib

--- src/pedigree/Coalescence.java
+++ src/pedigree/Coalescence.java
@@ -1,5 +1,6 @@
 package pedigree;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
@@ -7,12 +8,14 @@ import java.util.PriorityQueue;
 public class Coalescence {
 
 	private Simulation simulC = new Simulation();
-	private PriorityQueue<Sim> PA = new PriorityQueue<Sim>();
-	private HashMap<Double,Integer> PA_set = new HashMap<Double,Integer>();
+	private PriorityQueue<Sim> PA_m = new PriorityQueue<Sim>();
+	protected HashMap<Double,Integer> PA_set_m = new HashMap<Double,Integer>();
+	private PriorityQueue<Sim> PA_f = new PriorityQueue<Sim>();
+	protected HashMap<Double,Integer> PA_set_f = new HashMap<Double,Integer>();
 	
 	public void init() {
 		//On part la simulation pendant un certain temps
-		simulC.simulate(50, 200); //Arbitraire
+		simulC.simulate(30, 250); //Arbitraire
 		
 		//On copie la population finale pour chaque sexe de notre simulation
 		ArrayList<Sim> populationH = new ArrayList<Sim>();
@@ -29,14 +32,27 @@ public class Coalescence {
         //Tant qu'il reste plus d'un père
         while(populationH.size() > 1) {
         	//Si le père n'est pas encore dans l'ensemble PA, ajouter
-        	if(!PA.contains(getYoungest(populationH).getFather())) {
-        		PA.add(getYoungest(populationH));
+        	if(!PA_m.contains(getYoungest(populationH).getFather())) {
+        		PA_m.add(getYoungest(populationH));
         	//Si le père se retrouve déjà dans l'ensemble PA, créer un nouveau point de coalescence
         	} else {
-        		PA_set.put(getYoungest(populationH).getBirthTime(), PA.size());
+        		PA_set_m.put(getYoungest(populationH).getBirthTime(), PA_m.size());
         	}
         	//À chaque itération, retirer l'individu le plus jeune
         	populationH.remove(populationH.indexOf(getYoungest(populationH)));
+        }
+        
+      //Tant qu'il reste plus d'une mère
+        while(populationF.size() > 1) {
+        	//Si le père n'est pas encore dans l'ensemble PA, ajouter
+        	if(!PA_f.contains(getYoungest(populationF).getMother())) {
+        		PA_f.add(getYoungest(populationF));
+        	//Si le père se retrouve déjà dans l'ensemble PA, créer un nouveau point de coalescence
+        	} else {
+        		PA_set_f.put(getYoungest(populationF).getBirthTime(), PA_f.size());
+        	}
+        	//À chaque itération, retirer l'individu le plus jeune
+        	populationF.remove(populationF.indexOf(getYoungest(populationF)));
         }
 	}
 	
